dbd53ad66df4c4081ab3bbe4966be921d906639d
jimmyneutront <w8250693648@protonmail.ch>
2022-05-30 16:21:31 -0400

    Move to new package without "example" in name

--- androidApp/src/main/java/com/commuto/interfacemobile/android/MainActivity.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/MainActivity.kt
@@ -1,19 +1,15 @@
-package com.example.commuto_interface_mobile.android
+package com.commuto.interfacemobile.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 
-fun greet(): String {
-    return "Sup bro"
-}
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = greet())
+            Text(text = "Hey queen")
         }
     }
 }
