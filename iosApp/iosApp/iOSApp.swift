b75138e1c871b483470c8cb44ca0c5e90fb7000c
jimmyneutront <w8250693648@protonmail.ch>
2022-06-01 10:05:43 -0400

    Use dependency injection on iOS

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,13 +1,20 @@
 import SwiftUI
+import Swinject
 
 @main
 struct iOSApp: App {
     
-    let offersViewModel = OffersViewModel()
+    let container: Container
+    
+    init() {
+        container = Container()
+        container.register(OffersViewModel.self) {_ in OffersViewModel() }
+            .inObjectScope(.container)
+    }
     
 	var body: some Scene {
 		WindowGroup {
-            OffersView(offersViewModel: offersViewModel)
+            OffersView(offersViewModel: container.resolve(OffersViewModel.self)!)
 		}
 	}
 }
