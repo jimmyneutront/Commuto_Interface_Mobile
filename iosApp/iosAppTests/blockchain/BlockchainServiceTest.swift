b9974944b7c8620cc64b4cb837920555977d7478
jimmyneutront <w8250693648@protonmail.ch>
2022-06-07 15:19:10 -0400

    Test BlockchainService's listen method on iOS using the Commuto iOS Test Server

--- iosApp/iosAppTests/blockchain/BlockchainServiceTest.swift
+++ iosApp/iosAppTests/blockchain/BlockchainServiceTest.swift
@@ -27,4 +27,59 @@ class BlockchainServiceTest: XCTestCase {
         blockchainService.listenLoop()
     }
     
+    func testListen() {
+        
+        struct TestingServerResponse: Decodable {
+            let commutoSwapAddress: String
+        }
+        
+        let responseExpectation = XCTestExpectation(description: "Get new CommutoSwap contract address from testing server")
+        let invertedExpectation = XCTestExpectation(description: "Indicates something unexpected has happened, test should fail immediately")
+        invertedExpectation.isInverted = true
+        
+        let testingServerUrl = URL(string: "http://localhost:8546/test_blockchainservice_listen")!
+        var request = URLRequest(url: testingServerUrl)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        var testingServerResponse: TestingServerResponse? = nil
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print(error)
+                invertedExpectation.fulfill()
+            } else if let data = data {
+                testingServerResponse = try! JSONDecoder().decode(TestingServerResponse.self, from: data)
+                responseExpectation.fulfill()
+            } else {
+                print(response!)
+                invertedExpectation.fulfill()
+            }
+        }
+        task.resume()
+        wait(for: [responseExpectation, invertedExpectation], timeout: 60.0)
+        
+        let w3 = web3(provider: Web3HttpProvider(URL(string: "")!)!)
+        
+        class TestOfferService: OfferNotifiable {
+            init(_ oOE: XCTestExpectation, _ oTE: XCTestExpectation) {
+                offerOpenedExpectation = oOE
+                offerTakenExpectation = oTE
+            }
+            let offerOpenedExpectation: XCTestExpectation
+            let offerTakenExpectation: XCTestExpectation
+            func handleOfferOpenedEvent(_ event: OfferOpenedEvent) {
+                offerOpenedExpectation.fulfill()
+            }
+            func handleOfferTakenEvent(_ event: OfferTakenEvent) {
+                offerTakenExpectation.fulfill()
+            }
+        }
+        
+        let offerOpenedExpectation = XCTestExpectation(description: "handleOfferOpenedEvent was called")
+        let offerTakenExpectation = XCTestExpectation(description: "handleOfferTakenEvent was called")
+        let offerService = TestOfferService(offerOpenedExpectation, offerTakenExpectation)
+        
+        let blockchainService = BlockchainService(offerService: offerService, web3Instance: w3, commutoSwapAddress: testingServerResponse!.commutoSwapAddress)
+        blockchainService.listen()
+        wait(for: [offerOpenedExpectation, offerTakenExpectation], timeout: 60.0)
+    }
 }
