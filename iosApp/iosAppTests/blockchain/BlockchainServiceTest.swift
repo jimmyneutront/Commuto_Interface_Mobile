19b9c66a969d566eb5581ebe39637cc53c2bdf5c
jimmyneutront <w8250693648@protonmail.ch>
2022-06-09 21:34:42 -0400

    Test BlockchainService's listen loop error handling on iOS

--- iosApp/iosAppTests/blockchain/BlockchainServiceTest.swift
+++ iosApp/iosAppTests/blockchain/BlockchainServiceTest.swift
@@ -56,9 +56,8 @@ class BlockchainServiceTest: XCTestCase {
         }
         
         let responseExpectation = XCTestExpectation(description: "Get new CommutoSwap contract address from testing server")
-        let invertedExpectation = XCTestExpectation(description: "Indicates something unexpected has happened, test should fail immediately")
+        let invertedExpectation = XCTestExpectation(description: "Something unexpected happened")
         invertedExpectation.isInverted = true
-        
         let testingServerUrl = URL(string: "http://localhost:8546/test_blockchainservice_listen")!
         var request = URLRequest(url: testingServerUrl)
         request.httpMethod = "GET"
@@ -95,6 +94,7 @@ class BlockchainServiceTest: XCTestCase {
             }
         }
         let blockchainErrorExpectation = XCTestExpectation(description: "BlockchainService encountered an unexpected error")
+        blockchainErrorExpectation.isInverted = true
         let errorHandler = TestBlockchainErrorHandler(blockchainErrorExpectation)
         
         class TestOfferService: OfferNotifiable {
@@ -135,5 +135,73 @@ class BlockchainServiceTest: XCTestCase {
         wait(for: [offerOpenedExpectation, offerTakenExpectation, blockchainErrorExpectation], timeout: 60.0)
         XCTAssertEqual(expectedOfferId, try! offerService.offerOpenedEventPromise!.wait().id)
         XCTAssertEqual(expectedOfferId, try! offerService.offerTakenEventPromise!.wait().id)
+    }
+    
+    func testListenErrorHandling() {
+        
+        struct TestingServerResponse: Decodable {
+            let commutoSwapAddress: String
+        }
+        
+        let responseExpectation = XCTestExpectation(description: "Get fake CommutoSwap contract address from testing server")
+        let invertedExpectation = XCTestExpectation(description: "Something unexpected happened")
+        invertedExpectation.isInverted = true
+        let testingServerUrl = URL(string: "http://localhost:8546/test_blockchainservice_error_handling")!
+        var request = URLRequest(url: testingServerUrl)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        var testingServerResponse: TestingServerResponse? = nil
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print(error)
+                invertedExpectation.fulfill()
+            } else if let data = data {
+                testingServerResponse = try! JSONDecoder().decode(TestingServerResponse.self, from: data)
+                responseExpectation.fulfill()
+            } else {
+                print(response!)
+                invertedExpectation.fulfill()
+            }
+        }
+        task.resume()
+        wait(for: [responseExpectation, invertedExpectation], timeout: 5.0)
+        
+        /*
+         Yes, we want to create a web3 instance connected to the Commuto Testing Server, NOT a local Ethereum node. When a web3 instance is created, it attempts to call the "net_version" endpoint of the node it is to connect to. The Commuto Testing Server responds to this Ethereum endpoint in order to trick this web3 instance into thinking it is connected to a real Ethereum node, so that when it calls any other endpoint it throws an error and lets us test BlockchainService's error handling code.
+         */
+        let w3 = web3(provider: Web3HttpProvider(URL(string: "http://localhost:8546")!)!)
+        
+        class TestBlockchainErrorHandler: BlockchainErrorNotifiable {
+            init(_ eE: XCTestExpectation) {
+                errorExpectation = eE
+            }
+            var errorPromise: Promise<Error>? = nil
+            let errorExpectation: XCTestExpectation
+            func handleBlockchainError(_ error: Error) {
+                errorPromise = Promise { seal in
+                    seal.fulfill(error)
+                }
+                errorExpectation.fulfill()
+            }
+        }
+        let errorExpectation = XCTestExpectation(description: "We expect to get an error here")
+        let errorHandler = TestBlockchainErrorHandler(errorExpectation)
+        
+        class TestOfferService: OfferNotifiable {
+            func handleOfferOpenedEvent(_ event: OfferOpenedEvent) {
+            }
+            func handleOfferTakenEvent(_ event: OfferTakenEvent) {
+            }
+        }
+        
+        let blockchainService = BlockchainService(
+            errorHandler: errorHandler,
+            offerService: TestOfferService(),
+            web3Instance: w3,
+            commutoSwapAddress: testingServerResponse!.commutoSwapAddress
+        )
+        blockchainService.listen()
+        wait(for: [errorExpectation], timeout: 20.0)
+        assert(try! (errorHandler.errorPromise!.wait() as! Web3Error).errorDescription == "Node response is empty")
     }
 }
