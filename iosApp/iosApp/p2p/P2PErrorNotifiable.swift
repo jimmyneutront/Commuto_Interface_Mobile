ed85107619acd45943118308e63400689dcf75b2
jimmyneutront <w8250693648@protonmail.ch>
2022-06-18 13:01:38 -0400

    Document P2PErrorNotifiable on iOS

--- iosApp/iosApp/p2p/P2PErrorNotifiable.swift
+++ iosApp/iosApp/p2p/P2PErrorNotifiable.swift
@@ -6,6 +6,14 @@
 //  Copyright © 2022 orgName. All rights reserved.
 //
 
+/**
+ A protocol that a structure or class must adopt in order to be notified of `Error`s encountered by `P2PService`.
+ */
 protocol P2PErrorNotifiable {
+    /**
+     The function called by `P2PService` in order to notify the structure or class adopting this protocol of an `Error` encountered by  `P2PService`.
+     
+     - Parameter error: the `Error` encountered by `P2PService` of which  the structure or class adopting this protocol is being notified, and should handle in the implementation of this function.
+     */
     func handleP2PError(_ error: Error)
 }
