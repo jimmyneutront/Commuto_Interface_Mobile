6752b9bcf6d789fe5e95946fb1b614cff6d94e29
jimmyneutront <w8250693648@protonmail.ch>
2022-06-18 11:52:33 -0400

    Document P2PService on iOS

--- iosApp/iosApp/p2p/P2PService.swift
+++ iosApp/iosApp/p2p/P2PService.swift
@@ -11,37 +11,75 @@ import Foundation
 import PromiseKit
 import Switrix
 
+/**
+ The main Peer-to-Peer Service. It is responsible for listening to new messages in the Commuto Interface Network Matrix room, parsing the messages, and then passing relevant messages to other services as necessary.
+ */
 class P2PService {
     
+    /**
+     Initializes a new P2PService object.
+     
+     - Parameters:
+        - errorHandler: An object that conforms to the `P2PErrorNotifiable` protocol, to which this will pass errors when they occur.
+        - offerService: An object that comforms to the `OfferMessageNotifiable` protocol, to which this will pass offer-related messages.
+        - switrixClient: A `SwitrixClient` instance used to interact with a Matrix Homeserver.
+     
+     - Returns: A new `P2PService` instance.
+     */
     init(errorHandler: P2PErrorNotifiable, offerService: OfferMessageNotifiable, switrixClient: SwitrixClient) {
         self.errorHandler = errorHandler
         self.offerService = offerService
         self.switrixClient = switrixClient
     }
     
+    /**
+     An object to which `P2PService` will pass errors when they occur.
+     */
     private let errorHandler: P2PErrorNotifiable
     
+    /**
+     An object to which `P2PService` will pass offer-related messages when they occur.
+     */
     private let offerService: OfferMessageNotifiable
     
-    // Switrix client
+    /**
+     A `SwitrixClient` instance used to interact with a Matrix Homeserver.
+     */
     let switrixClient: SwitrixClient
     
     // The token at the end of the last batch of non-empty messages (this token is that from the beginning of the Commuto Interface network test room)
+    /**
+     The token at the end of the last batch of non-empty Matrix events that was parsed. (The value specified here is that from the beginning of the Commuto Interface Network testing room.) This should be updated every time a new batch of events is parsed.
+     */
     private var lastNonEmptyBatchToken = "t1-2607497254_757284974_11441483_1402797642_1423439559_3319206_507472245_4060289024_0"
     
+    /**
+     Used to update `lastNonEmptyBatchToken`. Eventually, this function will store `newToken` in persistent storage.
+     
+     - Parameter newToken: The batch token at the end of the most recently parsed batch of Matrix events, to be set as `lastNonEmptyBatchToken`.
+     */
     private func updateLastNonEmptyBatchToken(_ newToken: String) {
         lastNonEmptyBatchToken = newToken
     }
     
-    // The thread in which P2PService listens to the peer-to-peer network
+    /**
+     The thread in which P2PService listens for and parses messages.
+     */
     private var listenThread: Thread?
     
-    // The timer responsible for repeatedly beginning the listening process
+    /**
+     The timer that runs in `listenThread`'s `RunLoop` and is responsible for repeatedly executing the listening process.
+     */
     private var timer: Timer?
     
-    // Indicates whether P2PService is currently handling new data as part of the listening process
+    /**
+     Indicates whether P2PService is currently handling new data as part of the listening process.
+     */
     private var isDoingListening = false
     
+    /**
+     Creates a new  `Thread` to run `listenLoop()`, updates `listenThread` with a reference to the new `Thread`, and starts it.
+     */
     func listen() {
         listenThread = Thread { [self] in
             listenLoop()
@@ -49,11 +87,27 @@ class P2PService {
         listenThread!.start()
     }
     
+    /**
+     Invalidates the `Timer` resposible for repeatedly starting the listening process and cancels the `listenThread`.
+     */
     func stopListening() {
         timer?.invalidate()
         listenThread?.cancel()
     }
     
+    /**
+     Creates a new `Timer` responsible for repeatedly executing the listening process once approximately every second, adds the `Timer` to the current thread's `Runloop`, and then starts said `Runloop`.
+     
+     Listening Process:
+     
+     First,  we check the`isDoingListening` flag. If `isDoingListening` is true, then the listening process is currently running, so we don't start it again. If `isDoingListening` is false, then the listening process isn't currently running, so we can safely start it.
+     
+     Once we have established that we can safely start the listening process, we call `syncPromise()` to get the most recent batch token from the Matrix homeserver. Then we try to get all new events since that most recent batch token, with a limit of one trillion events, by calling `getMessagesPromise()`. We update our last non-empty batch token with the `nextBatchToken` value in the response from the `syncPromise()` call, and then we parse the new events for relavent messages.
+     
+     Finally, once the listening process is completed, we set `isDoingListening` to `false` to indicate that the listening process is not currently running and can be started again by the `Timer`.
+     
+     If we encounter an error, we pass it to our `errorHandler`. Additionally, if the error indicates that we have a missing or invalid token, we call `stopListening()` to prevent the listening process from being started again, since the user must perform some action in order to resolve this issue.
+     */
     func listenLoop() {
         let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] _ in
             if (!isDoingListening) {
@@ -90,6 +144,11 @@ class P2PService {
         RunLoop.current.run()
     }
     
+    /**
+     A `Promise` wrapper around Switrix's `SwitrixSyncClient().sync()` method.
+     
+     - Returns: A `Promise<SwitrixSyncResponse>`, in which the `SwitrixSyncResponse` is from the Commuto Interface Network test room.
+     */
     private func syncPromise() -> Promise<SwitrixSyncResponse> {
         return Promise { seal in
             switrixClient.sync.sync { response in
@@ -104,6 +163,15 @@ class P2PService {
     }
     
     #warning("TODO: would be nice to specify to parameter here")
+    /**
+     A `Promise` wrapper around Switrix's `SwitrixEventsClient().getEvents(...)` method. This method gets message events only from the Commuto Interface Network test room.
+     
+     - Parameters:
+        - from: The batch token from which to start returning events.
+        - limit: The maximun number of events to return. Note that this will likely be greater than the number of events actually returned, since Switrix filters out all events that are not text message events.
+     
+     - Returns: A  `Promise<SwitrixGetEventsResponse>`, in which the `SwitrixGetEventsResponse`, if successful, contains text message events from the Commuto Interface Network test room.
+     */
     private func getMessagesPromise(from: String, limit: Int) -> Promise<SwitrixGetEventsResponse> {
         return Promise { seal in
             switrixClient.events.getEvents(roomId: "!WEuJJHaRpDvkbSveLu:matrix.org", from: from, direction: .forwards, limit: limit) { response in
@@ -117,6 +185,11 @@ class P2PService {
         }
     }
     
+    /**
+     Parses an array of `SwitrixClientEvent`s, filters out all non-message events, attempts to create message objects from the content bodies of message events, if present, and then passes any created offer-related message objects to `offerService`.
+     
+     - Parameter events: a list of `SwitrixClientEvent` objects.
+     */
     private func parseEvents(_ events: [SwitrixClientEvent]) {
         let messageEvents = events.filter { event in
             return event.type == "m.room.message"
@@ -129,6 +202,14 @@ class P2PService {
     
     }
     
+    #warning("This shouldn't throw, it should just return nil")
+    /**
+     Attempts to restore a PublicKeyAnnouncement from a given string.
+     
+     - Parameter messageString: A `String?` from which to try to restore a `PublicKeyAnnouncement`. If `messageString` is nil, `parsePublicKeyAnnouncement(...)` immediately returns nil.
+     
+     - Returns: A `PublicKeyAnnouncement?` that will be `nil` if `messageString` does not contain a valid public key announcement, and will be non-`nil` if `messageString` does contain a valid public key announcement.
+     */
     private func parsePublicKeyAnnouncement(messageString: String?) throws -> PublicKeyAnnouncement? {
         guard messageString != nil else {
             return nil
