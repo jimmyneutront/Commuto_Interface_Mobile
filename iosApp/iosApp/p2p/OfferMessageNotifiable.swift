81c138879407854db125a0f6666638cfef4b1eac
jimmyneutront <w8250693648@protonmail.ch>
2022-06-18 12:48:13 -0400

    Document OfferMessageNotifiable on iOS

--- iosApp/iosApp/p2p/OfferMessageNotifiable.swift
+++ iosApp/iosApp/p2p/OfferMessageNotifiable.swift
@@ -6,6 +6,14 @@
 //  Copyright © 2022 orgName. All rights reserved.
 //
 
+/**
+ A protocol that a structure or class must adopt in order to receive offer-related messages from `P2PService`.
+ */
 protocol OfferMessageNotifiable {
+    /**
+     The function called by `P2PService` in order to notify the structure or class adopting this protocol of a new `PublicKeyAnnouncement`.
+     
+     - Parameter message: the new `PublicKeyAnnouncement` of which the structure or class adopting this protocol is being notified, and should handle in the implementation of this function.
+     */
     func handlePublicKeyAnnouncement(_ message: PublicKeyAnnouncement)
 }
