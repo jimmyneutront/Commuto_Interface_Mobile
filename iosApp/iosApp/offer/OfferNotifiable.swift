20151bf1dc3ec2ef10751d7426a2fdde5b71c314
jimmyneutront <w8250693648@protonmail.ch>
2022-06-19 15:44:17 -0400

    Document OfferNotifiable on iOS

--- iosApp/iosApp/offer/OfferNotifiable.swift
+++ iosApp/iosApp/offer/OfferNotifiable.swift
@@ -6,12 +6,29 @@
 //  Copyright © 2022 orgName. All rights reserved.
 //
 
+/**
+ A protocol that a structure or class must adopt in order to be notified of offer-related blockchain events by `BlockchainService`.
+ */
 protocol OfferNotifiable {
-    
+    /**
+     The function called by `BlockchainService` in order to notify the structure or class adopting this protocol of an `OfferOpenedEvent`.
+     
+     - Parameter event: The `OfferOpenedEvent` of which the structure or class adopting this protocol is being notified and should handle in the implementation of  this function.
+     */
     func handleOfferOpenedEvent(_ event: OfferOpenedEvent)
     
+    /**
+     The function called by `BlockchainService` in order to notify the structure or class adopting this protocol of an `OfferCanceledEvent`.
+     
+     - Parameter event: The `OfferCanceledEvent` of which the structure or class adopting this protocol is being notified and should handle in the implementation of  this function.
+     */
     func handleOfferCanceledEvent(_ event: OfferCanceledEvent)
     
+    /**
+     The function called by `BlockchainService` in order to notify the structure or class adopting this protocol of an `OfferTakenEvent`.
+     
+     - Parameter event: The `OfferTakenEvent` of which the structure or class adopting this protocol is being notified and should handle in the implementation of  this function.
+     */
     func handleOfferTakenEvent(_ event: OfferTakenEvent)
     
 }
