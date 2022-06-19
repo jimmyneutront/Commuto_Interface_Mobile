5b2516dba5967382729cb3b0d6af38678675cb1b
jimmyneutront <w8250693648@protonmail.ch>
2022-06-18 20:39:04 -0400

    Document BlockchainServiceError on iOS

--- iosApp/iosApp/blockchain/BlockchainServiceError.swift
+++ iosApp/iosApp/blockchain/BlockchainServiceError.swift
@@ -6,8 +6,20 @@
 //  Copyright © 2022 orgName. All rights reserved.
 //
 
+/**
+ An `Error` thrown by `BlockchainService` functions.
+ */
 enum BlockchainServiceError: Error {
+    /**
+    Thrown when a `BlockchainService` function receives `nil` from a call that should return a non-`nil` result.
+     
+     - Parameter desc: A `String` that provides information about the context in which the error was thrown.
+     */
     case unexpectedNilError(desc: String)
+    
+    /**
+     A description providing information about the context in which the error was thrown.
+     */
     public var errorDescription: String {
         switch self {
         case .unexpectedNilError(let desc):
