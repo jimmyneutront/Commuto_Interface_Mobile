e288a293f25835a017e923aca803c0ccd78bc4c6
jimmyneutront <w8250693648@protonmail.ch>
2022-06-19 09:23:53 -0400

    Document OffersViewModel on iOS

--- iosApp/iosApp/ui/OffersViewModel.swift
+++ iosApp/iosApp/ui/OffersViewModel.swift
@@ -8,13 +8,29 @@
 
 import Foundation
 
+/**
+ The Offers View Model, the single source of truth for all offer related data. It is observed by offer-related views.
+ */
 class OffersViewModel: ObservableObject {
     
-    let offerService: OfferService
-    @Published var offers: [UUID: Offer]
-    
+    /**
+     Initializes a new `OffersViewModel`.
+     
+     - Parameter offerService: An `OfferService` that adds and removes `Offer`s from this class's `offers` dictionary as offers are created, canceled and taken.
+     */
     init(offerService: OfferService) {
         self.offerService = offerService
         offers = Offer.sampleOffers
     }
+    
+    /**
+     The `OfferService` responsible for adding and removing `Offers` from this class's `offers` dictionary as offers are created, canceled and taken.
+     */
+    let offerService: OfferService
+    
+    /**
+     A dictionary mapping offer IDs (as `UUID`s) to `Offer`s, which is the single source of truth for all open-offer-related data.
+     */
+    @Published var offers: [UUID: Offer]
+    
 }
