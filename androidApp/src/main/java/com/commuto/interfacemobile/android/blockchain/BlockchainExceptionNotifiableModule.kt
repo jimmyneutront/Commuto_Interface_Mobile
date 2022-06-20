6442b7fa976b754e85298b85d2c157c85a010e20
jimmyneutront <w8250693648@protonmail.ch>
2022-06-20 14:18:18 -0400

    Document BlockchainExceptionNotifiableModule on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/blockchain/BlockchainExceptionNotifiableModule.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/blockchain/BlockchainExceptionNotifiableModule.kt
@@ -6,9 +6,17 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+/**
+ * A Dagger [Module] that tells Dagger what to inject into objects that depend on an object
+ * implementing [BlockchainExceptionNotifiable]
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 interface BlockchainExceptionNotifiableModule {
+    /**
+     * A Dagger Binding that tells Dagger to inject an instance of [ExceptionViewModel] into objects
+     * that depend on an object implementing [BlockchainExceptionNotifiable]
+     */
     @Binds
     fun bindBlockchainExceptionNotifiable(impl: ExceptionViewModel): BlockchainExceptionNotifiable
 }
