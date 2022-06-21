d805bf57e2f87427899a0dec899312cd8faf4f3b
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 09:12:44 -0400

    Document OfferMessageNotifiable on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/p2p/OfferMessageNotifiable.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/p2p/OfferMessageNotifiable.kt
@@ -2,6 +2,17 @@ package com.commuto.interfacemobile.android.p2p
 
 import com.commuto.interfacemobile.android.p2p.messages.PublicKeyAnnouncement
 
+/**
+ * An interface that a class must implement in order to be notified of offer-related messages by
+ * [P2PService].
+ */
 interface OfferMessageNotifiable {
+    /**
+     * The method called by [P2PService] in order to notify the class implementing this interface of
+     * a new [PublicKeyAnnouncement].
+     *
+     * @param message The new [PublicKeyAnnouncement] of which the class implementing this interface
+     * is being notified and should handle in the implementation of this method.
+     */
     fun handlePublicKeyAnnouncement(message: PublicKeyAnnouncement)
 }
