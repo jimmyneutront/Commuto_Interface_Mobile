9818a00642bfd4c80e9dd00bc28d77c61df56ce3
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 10:20:36 -0400

    Document ExceptionViewModel on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/ui/ExceptionViewModel.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/ui/ExceptionViewModel.kt
@@ -2,11 +2,23 @@ package com.commuto.interfacemobile.android.ui
 
 import androidx.lifecycle.ViewModel
 import com.commuto.interfacemobile.android.blockchain.BlockchainExceptionNotifiable
+import com.commuto.interfacemobile.android.blockchain.BlockchainService
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * The Exception View Model, used to display a visual notification to the user when unexpected
+ * exceptions occur.
+ */
 @Singleton
 class ExceptionViewModel @Inject constructor(): ViewModel(), BlockchainExceptionNotifiable {
+    /**
+     * The method called by [BlockchainService] in order to notify of an [Exception] encountered
+     * by [BlockchainService].
+     *
+     * @param exception The [Exception] encountered by [BlockchainService] about which this method
+     * should notify the user.
+     */
     override fun handleBlockchainException(exception: Exception) {
         throw exception
     }
