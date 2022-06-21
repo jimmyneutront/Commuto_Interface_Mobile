97030b6fabd4d7b087207d56cdbd27e1eb6037d4
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 12:20:36 -0400

    Document OffersViewModel on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/ui/OffersViewModel.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/ui/OffersViewModel.kt
@@ -5,5 +5,13 @@ import com.commuto.interfacemobile.android.offer.OfferService
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * The Offers View Model, the single source of truth for all offer-related data. It is observed by
+ * offer-related [androidx.compose.runtime.Composable]s.
+ *
+ * @property offerService The [OfferService] responsible for adding and removing
+ * [com.commuto.interfacemobile.android.offer.Offer]s from the list of open offerss as they are
+ * created, canceled and taken.
+ */
 @Singleton
 class OffersViewModel @Inject constructor(val offerService: OfferService): ViewModel()
