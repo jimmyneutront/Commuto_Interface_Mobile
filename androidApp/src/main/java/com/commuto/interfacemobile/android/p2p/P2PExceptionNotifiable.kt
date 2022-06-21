0ba3f136535e6fab5abfc3f205f82e58857c3e2a
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 09:18:53 -0400

    Document P2PExceptionNotifiable on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/p2p/P2PExceptionNotifiable.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/p2p/P2PExceptionNotifiable.kt
@@ -2,7 +2,18 @@ package com.commuto.interfacemobile.android.p2p
 
 import javax.inject.Singleton
 
+/**
+ * An interface that a class must implement in order to be notified of [Exception]s encountered by
+ * [P2PService].
+ */
 @Singleton
 interface P2PExceptionNotifiable {
+    /**
+     * The method called by [P2PService] in order to notify the class implementing this interface of
+     * an [Exception] encountered by [P2PService].
+     *
+     * @param exception The [Exception] encountered by [P2PService] of which the class implementing
+     * this interface is being notified and should handle in the implementation of this method.
+     */
     fun handleP2PException(exception: Exception)
 }
