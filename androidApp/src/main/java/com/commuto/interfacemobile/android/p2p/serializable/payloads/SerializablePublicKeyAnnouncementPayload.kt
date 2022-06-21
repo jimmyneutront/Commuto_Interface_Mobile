d65264b91b3468a306ba013ffe198b8d7e082f30
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 00:27:11 -0400

    Documented SerializablePublicKeyAnnouncementPayload on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/p2p/serializable/payloads/SerializablePublicKeyAnnouncementPayload.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/p2p/serializable/payloads/SerializablePublicKeyAnnouncementPayload.kt
@@ -2,6 +2,13 @@ package com.commuto.interfacemobile.android.p2p.serializable.payloads
 
 import kotlinx.serialization.Serializable
 
+/**
+ * A [Public Key Announcement payload](https://github.com/jimmyneutront/commuto-whitepaper/blob/main/commuto-interface-specification.txt#L58)
+ *
+ * @param pubKey The public key being announced, as a [String].
+ * @param offerId The ID of the offer for which this Public Key Announcement is being made, as a
+ * [String].
+ */
 @Serializable
 data class SerializablePublicKeyAnnouncementPayload(
     val pubKey: String,
