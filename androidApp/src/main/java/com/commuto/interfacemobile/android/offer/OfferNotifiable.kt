10deb27ecf981da78a5f2bd6d37f6a741b7502f7
jimmyneutront <w8250693648@protonmail.ch>
2022-06-20 23:52:55 -0400

    Document OfferNotifiable on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/offer/OfferNotifiable.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/offer/OfferNotifiable.kt
@@ -3,9 +3,42 @@ package com.commuto.interfacemobile.android.offer
 import com.commuto.interfacemobile.android.CommutoSwap
 import javax.inject.Singleton
 
+/**
+ * An interface that a class must implement in order to be notified of offer-related blockchain
+ * events by [com.commuto.interfacemobile.android.blockchain.BlockchainService].
+ */
 @Singleton
 interface OfferNotifiable {
+    /**
+     * The method called by [com.commuto.interfacemobile.android.blockchain.BlockchainService] in
+     * order to notify the class implementing this interface of a
+     * [CommutoSwap.OfferOpenedEventResponse].
+     *
+     * @param offerOpenedEventResponse The [CommutoSwap.OfferOpenedEventResponse] of which the class
+     * implementing this interface is being notified and should handle in the implementation of this
+     * method.
+     */
     fun handleOfferOpenedEvent(offerOpenedEventResponse: CommutoSwap.OfferOpenedEventResponse)
+
+    /**
+     * The method called by [com.commuto.interfacemobile.android.blockchain.BlockchainService] in
+     * order to notify the class implementing this interface of a
+     * [CommutoSwap.OfferCanceledEventResponse].
+     *
+     * @param offerCanceledEventResponse The [CommutoSwap.OfferCanceledEventResponse] of which the
+     * class implementing this interface is being notified and should handle in the implementation
+     * of this method.
+     */
     fun handleOfferCanceledEvent(offerCanceledEventResponse: CommutoSwap.OfferCanceledEventResponse)
+
+    /**
+     * The method called by [com.commuto.interfacemobile.android.blockchain.BlockchainService] in
+     * order to notify the class implementing this interface of a
+     * [CommutoSwap.OfferTakenEventResponse].
+     *
+     * @param offerTakenEventResponse The [CommutoSwap.OfferTakenEventResponse] of which the class
+     * implementing this interface is being notified and should handle in the implementation of this
+     * method.
+     */
     fun handleOfferTakenEvent(offerTakenEventResponse: CommutoSwap.OfferTakenEventResponse)
 }
