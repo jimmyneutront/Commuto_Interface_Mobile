ffca950d1bab50198750dcdcd29cc2983837efd9
jimmyneutront <w8250693648@protonmail.ch>
2022-06-21 00:13:57 -0400

    Document PublicKeyAnnouncement on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/p2p/messages/PublicKeyAnnouncement.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/p2p/messages/PublicKeyAnnouncement.kt
@@ -3,4 +3,10 @@ package com.commuto.interfacemobile.android.p2p.messages
 import com.commuto.interfacemobile.android.keymanager.types.PublicKey
 import java.util.*
 
+/**
+ * This represents a [Public Key Announcement](https://github.com/jimmyneutront/commuto-whitepaper/blob/main/commuto-interface-specification.txt#L50).
+ *
+ * @param id The ID of the offer corresponding to this [PublicKeyAnnouncement].
+ * @param publicKey The [PublicKey] that this [PublicKeyAnnouncement] is announcing.
+ */
 data class PublicKeyAnnouncement constructor(val id: UUID, val publicKey: PublicKey)
