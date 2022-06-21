58a0c6cc6241c72d17ed63a7d23de77ce282e315
jimmyneutront <w8250693648@protonmail.ch>
2022-06-20 23:56:04 -0400

    Document OfferNotifiableModule on Android

--- androidApp/src/main/java/com/commuto/interfacemobile/android/offer/OfferNotifiableModule.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/offer/OfferNotifiableModule.kt
@@ -5,9 +5,20 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+/**
+ * A Dagger [Module] that tells Dagger what to inject into objects that depend on an object
+ * implementing [OfferNotifiable]
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 interface OfferNotifiableModule {
+    /**
+     * A Dagger Binding that tells Dagger to inject an instance of [OfferService] into objects
+     * that depend on an object implementing [OfferNotifiable].
+     *
+     * @param impl The type of object that will be injected into objects that depend on an object
+     * implementing [OfferNotifiable].
+     */
     @Binds
     fun bindOfferNotifiable(impl: OfferService): OfferNotifiable
 }
