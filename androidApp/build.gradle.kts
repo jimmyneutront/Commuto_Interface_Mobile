bf9d50fffdb5064d124fdb406fe68d4e5171e346
jimmyneutront <w8250693648@protonmail.ch>
2022-07-16 16:58:30 -0400

    (Android) Update Jetpack Compose dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,11 +17,11 @@ fun trixnity(module: String, version: String = trixnityVersion) =
 
 dependencies {
     //implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     // Integration with activities
-    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.activity:activity-compose:1.5.0")
     // Compose Material Design
     implementation("androidx.compose.material:material:1.1.1")
     // Animations
@@ -29,7 +29,7 @@ dependencies {
     // Tooling support (Previews, etc.)
     implementation("androidx.compose.ui:ui-tooling:1.1.1")
     // Integration with ViewModels
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0")
     // UI Tests
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
     // Navigation
