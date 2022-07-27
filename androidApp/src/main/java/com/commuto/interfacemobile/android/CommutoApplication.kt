d26725ea1620f3f1c5563167b978044217fb5b31
jimmyneutront <w8250693648@protonmail.ch>
2022-07-27 09:23:09 -0400

    (Android) Create database tables at startup

--- androidApp/src/main/java/com/commuto/interfacemobile/android/CommutoApplication.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/CommutoApplication.kt
@@ -2,6 +2,7 @@ package com.commuto.interfacemobile.android
 
 import android.app.Application
 import com.commuto.interfacemobile.android.blockchain.BlockchainService
+import com.commuto.interfacemobile.android.database.DatabaseService
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -15,9 +16,13 @@ class CommutoApplication: Application() {
 
     @Inject
     lateinit var blockchainService: BlockchainService
+    @Inject
+    lateinit var databaseService: DatabaseService
 
     override fun onCreate() {
         super.onCreate()
+        // TODO: ONLY do this when using an in memory database, NOT when using a production database!
+        databaseService.createTables()
         // Start listening to the blockchain
         blockchainService.listen()
     }
