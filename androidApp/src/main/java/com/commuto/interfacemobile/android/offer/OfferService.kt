bf72d7e237c0418da7741aa6272edf321987e08a
jimmyneutront <w8250693648@protonmail.ch>
2022-08-20 23:44:27 -0400

    (Android) Prevent canceling of taken offers

--- androidApp/src/main/java/com/commuto/interfacemobile/android/offer/OfferService.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/offer/OfferService.kt
@@ -290,6 +290,9 @@ class OfferService (
      *
      * @param offerID The ID of the Offer to be canceled.
      * @param chainID The ID of the blockchain on which the Offer exists.
+     *
+     * @throws [OfferServiceException] if there is an offer in [offerTruthSource] with an ID equal to [offerID] and its
+     * [Offer.isTaken] value is false.
      */
     suspend fun cancelOffer(
         offerID: UUID,
@@ -297,6 +300,9 @@ class OfferService (
     ) {
         withContext(Dispatchers.IO) {
             Log.i(logTag, "cancelOffer: canceling $offerID")
+            if (offerTruthSource.offers[offerID]?.isTaken?.value == false) {
+                throw OfferServiceException(message = "Offer $offerID is taken and cannot be canceled.")
+            }
             try {
                 Log.i(logTag, "cancelOffer: persistently updating offer $offerID state to " +
                         OfferState.CANCELING.asString)
