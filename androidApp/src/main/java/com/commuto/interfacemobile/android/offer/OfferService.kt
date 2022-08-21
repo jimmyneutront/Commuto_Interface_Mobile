c621fe22b89ada395a9e2e0cba73f38a59d06fc0
jimmyneutront <w8250693648@protonmail.ch>
2022-08-21 00:46:03 -0400

    (Android) Prevent editing of taken offers

--- androidApp/src/main/java/com/commuto/interfacemobile/android/offer/OfferService.kt
+++ androidApp/src/main/java/com/commuto/interfacemobile/android/offer/OfferService.kt
@@ -356,6 +356,11 @@ class OfferService (
     suspend fun editOffer(offerID: UUID, newSettlementMethods: List<SettlementMethod>) {
         withContext(Dispatchers.IO) {
             Log.i(logTag, "editOffer: editing $offerID")
+            val offer = offerTruthSource.offers[offerID]
+                ?: throw OfferServiceException(message = "Offer $offerID was not found.")
+            if (offer.isTaken.value) {
+                throw OfferServiceException(message = "Offer $offerID is already taken.")
+            }
             try {
                 Log.i(logTag, "editOffer: serializing settlement methods for $offerID")
                 val onChainSettlementMethods: List<ByteArray> = try {
